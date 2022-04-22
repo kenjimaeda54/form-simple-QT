1ecc6ac492f1ee81e8b7d06f0492854d8b5fc7e4
kenjimaeda54 <kenjimaedafamily@gmail.com>
2022-04-22 11:06:52 -0300

    first commit

---
+++ form.cpp
@@ -0,0 +1,15 @@
+#include "form.h"
+#include "ui_form.h"
+
+Form::Form(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::Form)
+{
+    ui->setupUi(this);
+}
+
+Form::~Form()
+{
+    delete ui;
+}
+
---
+++ form.h
@@ -0,0 +1,21 @@
+#ifndef FORM_H
+#define FORM_H
+
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class Form; }
+QT_END_NAMESPACE
+
+class Form : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    Form(QWidget *parent = nullptr);
+    ~Form();
+
+private:
+    Ui::Form *ui;
+};
+#endif // FORM_H
